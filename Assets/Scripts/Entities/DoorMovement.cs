ed7aaa4529cec09f34f058db81f867289ca6405c
Magman3000 <112540778+Magman3000@users.noreply.github.com>
2024-01-25 14:13:43 +0100

    DoorStuff
    DoorStuff

---
+++ Assets/Scripts/Entities/DoorMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour
+{
+    public enum ObstacleState
+    {
+        error, active, inactive
+    }
+    [SerializeField] Vector3 secondPosition;
+    [SerializeField] Vector3 secondRotation;
+    Vector3 position;
+    Quaternion rotation;
+    ObstacleState doorState = new ObstacleState();
+    // Start is called before the first frame update
+    void Initialize()
+    {
+        doorState = ObstacleState.inactive;
+        position = gameObject.transform.position;
+
+    }
+
+    public void StateChange(ObstacleState state)
+    {
+        doorState = state; 
+        if(doorState == ObstacleState.active)
+        {
+            transform.Rotate(secondRotation);
+            transform.position = secondPosition;
+        }
+        else if(doorState == ObstacleState.inactive)
+        {
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+        else if(doorState == ObstacleState.error)
+        {
+            ErrorManager.Error(gameObject);
+        }
+    }
+}
