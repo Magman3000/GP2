8a47b4c30b3d67ddee6d77d4d5e4d0404ef23419
Magman3000 <112540778+Magman3000@users.noreply.github.com>
2024-01-25 15:30:26 +0100

    Update DoorMovement.cs

--- Assets/Scripts/Entities/DoorMovement.cs
+++ Assets/Scripts/Entities/DoorMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
@@ -11,33 +12,68 @@ public class DoorMovement : MonoBehaviour
     }
     [SerializeField] Vector3 secondPosition;
     [SerializeField] Vector3 secondRotation;
-    Vector3 position;
-    Quaternion rotation;
+    [SerializeField] float interpolationRatio;
+    [SerializeField] float interpolationLimit;
+    public Boolean moving = false;
+    Vector3 initialPosition;
+    Vector3 initialRotation;
     ObstacleState doorState = new ObstacleState();
     // Start is called before the first frame update
     void Initialize()
     {
         doorState = ObstacleState.inactive;
-        position = gameObject.transform.position;
+        initialRotation = gameObject.transform.rotation.eulerAngles;
+        initialPosition = gameObject.transform.position;
 
+    }
+    private void Update()
+    {
+        if (doorState == ObstacleState.active && moving)
+        {
+            Vector3 postionCalculation = Vector3.Lerp(transform.position, secondPosition, interpolationRatio * Time.deltaTime);
+            transform.position = postionCalculation;
+            Vector3 rotationCalculation = Vector3.Lerp(transform.eulerAngles, secondRotation, interpolationRatio * Time.deltaTime);
+            transform.eulerAngles = rotationCalculation;
+            float positionDistance = Vector3.Distance(postionCalculation, secondPosition);
+            float rotationDistance = Vector3.Distance(rotationCalculation, secondRotation);
+            if (positionDistance < interpolationLimit && rotationDistance < interpolationLimit)
+            {
+                transform.position = secondPosition;
+                transform.eulerAngles = secondRotation;
+                moving = false;
+            }
+        }
+        else if (doorState == ObstacleState.inactive && moving)
+        {
+            Vector3 postionCalculation = Vector3.Lerp(transform.position, initialPosition, interpolationRatio * Time.deltaTime);
+            transform.position = postionCalculation;
+            Vector3 rotationCalculation = Vector3.Lerp(transform.eulerAngles, initialRotation, interpolationRatio * Time.deltaTime);
+            transform.eulerAngles = rotationCalculation;
+            float positionDistance = Vector3.Distance(postionCalculation, initialPosition);
+            float rotationDistance = Vector3.Distance(rotationCalculation, initialRotation);
+            if (positionDistance < interpolationLimit && rotationDistance < interpolationLimit)
+            {
+                transform.position = initialPosition;
+                transform.eulerAngles = initialRotation;
+                moving = false;
+            }
+        }
     }
 
     public void StateChange(ObstacleState state)
     {
-        doorState = state; 
-        if(doorState == ObstacleState.active)
+        doorState = state;
+        if (doorState == ObstacleState.active)
         {
-            transform.eulerAngles = secondRotation;
-            transform.position = secondPosition;
+            moving = true;
         }
-        else if(doorState == ObstacleState.inactive)
+        else if (doorState == ObstacleState.inactive)
         {
-            transform.rotation = rotation;
-            transform.position = position;
+            moving = true;
         }
-        else if(doorState == ObstacleState.error)
+        else if (doorState == ObstacleState.error)
         {
-            ErrorManager.Error(gameObject);
+            Debug.Log("Error");
         }
     }
 }
