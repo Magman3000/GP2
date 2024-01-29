7863a488e1d26f89618afce99124f895d711c0a2
Gammastrale <90925619+Gammastrale@users.noreply.github.com>
2024-01-29 10:50:49 +0100

    update changes monday pre-lunch

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -2,14 +2,17 @@ using Unity.VisualScripting;
 using UnityEngine;
 using static MyUtility.Utility;
 
-public class Player : NetworkedEntity {
-    public enum PlayerIdentity {
+public class Player : NetworkedEntity
+{
+    public enum PlayerIdentity
+    {
         NONE = 0,
         DAREDEVIL, //Daredevil
         COORDINATOR //Coordinator
     }
 
     [SerializeField] private DaredevilStats daredevilStats;
+    [SerializeField] private CoordinatorStats coordinatorStats;
 
     private PlayerIdentity assignedPlayerIdentity = PlayerIdentity.NONE;
 
@@ -19,10 +22,13 @@ public class Player : NetworkedEntity {
     private CoordinatorHUD coordinatorHUD;
     private DaredevilHUD daredevilHUD;
 
-    public Rigidbody rigidbodyComp;
+    private Rigidbody rigidbodyComp;
+
+    private bool speedBoostBool = false;
 
 
-    public override void Initialize(GameInstance game) {
+    public override void Initialize(GameInstance game)
+    {
         if (initialized)
             return;
 
@@ -35,45 +41,55 @@ public class Player : NetworkedEntity {
         gameInstanceRef = game;
         initialized = true;
     }
-    public override void Tick() {
-        if (!initialized) {
+    public override void Tick()
+    {
+        if (!initialized)
+        {
             Warning("Attempted to tick player before it was initialized!");
             return;
         }
 
-        if (assignedPlayerIdentity == PlayerIdentity.DAREDEVIL) {
+        if (assignedPlayerIdentity == PlayerIdentity.DAREDEVIL)
+        {
             daredevilData.Tick();
             daredevilHUD.Tick();
         }
-        else if (assignedPlayerIdentity == PlayerIdentity.COORDINATOR) {
+        else if (assignedPlayerIdentity == PlayerIdentity.COORDINATOR)
+        {
             coordinatorData.Tick();
             coordinatorHUD.Tick();
         }
     }
-    public override void FixedTick() {
-        if (!initialized) {
+    public override void FixedTick()
+    {
+        if (!initialized)
+        {
             Warning("Attempted to fixed tick player before it was initialized!");
             return;
         }
 
-        if (assignedPlayerIdentity == PlayerIdentity.DAREDEVIL) {
+        if (assignedPlayerIdentity == PlayerIdentity.DAREDEVIL)
+        {
             daredevilData.FixedTick();
             daredevilHUD.FixedTick();
         }
-        else if (assignedPlayerIdentity == PlayerIdentity.COORDINATOR) {
+        else if (assignedPlayerIdentity == PlayerIdentity.COORDINATOR)
+        {
             coordinatorData.FixedTick();
             coordinatorHUD.FixedTick();
         }
     }
 
 
-    public void AssignPlayerIdentity(PlayerIdentity playerIdentity) {
+    public void AssignPlayerIdentity(PlayerIdentity playerIdentity)
+    {
         assignedPlayerIdentity = playerIdentity;
     }
     public void SetDaredevilHUD(DaredevilHUD hud) { daredevilHUD = hud; }
     public void SetCoordinatorHUD(CoordinatorHUD hud) { coordinatorHUD = hud; }
 
-    private void SetupReference() {
+    private void SetupReference()
+    {
         rigidbodyComp = GetComponent<Rigidbody>();
         Validate(rigidbodyComp, "Failed to get reference to Rigidbody component!", ValidationLevel.ERROR, true);
     }
@@ -81,7 +97,14 @@ public class Player : NetworkedEntity {
 
 
     public DaredevilStats GetDaredevilStats() { return daredevilStats; }
-    //Coordinator stats getter
+    public CoordinatorStats GetCoordinatorStats() { return coordinatorStats; }
     public Daredevil GetDaredevilData() { return daredevilData; }
     public Coordinator GetCoordinatorData() { return coordinatorData; }
+
+    public Rigidbody GetRigidbody() { return rigidbodyComp; }
+
+
+    public bool GetBoostCheck() { return speedBoostBool; }
+    public void SetBoostCheck(bool boostCheck) {  speedBoostBool = boostCheck; }
+
 }
---
+++ Assets/Scripts/ScriptableObjects/CoordinatorStats.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "CoordinatorStats", menuName = "Player/CoordinatorStats", order = 0)]
+public class CoordinatorStats : ScriptableObject
+{
+    [Range(0.1f, 500.0f)][SerializeField] private float boostDuration = 1.0f;
+    [Tooltip("The amount of boost charges")]
+    [Range(0.1f, 500.0f)][SerializeField] private int boostCharges = 1;
+
+
+
+    public float GetBoostDuration() { return boostDuration; }
+    public int GetBoostCharges() { return boostCharges; }
+}
