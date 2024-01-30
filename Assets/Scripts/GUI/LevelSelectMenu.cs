e3b2db572d612790453a8b5b1b11278ba72aeeb6
P3Ti009 <gulyaspeter20@gmail.com>
2024-01-30 16:04:39 +0100

    LevelSelectMenu

--- Assets/Scripts/GUI/LevelSelectMenu.cs
+++ Assets/Scripts/GUI/LevelSelectMenu.cs
@@ -1,49 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using static MyUtility.Utility;
 
-public class LevelSelectMenu : Entity {
+public class LevelSelectMenu : Entity
+{
+    [SerializeField] private LevelsBundle levelsBundle;
+    
+    public bool player1Ready = false;
+    public bool player2Ready = false;
 
-    public override void Initialize(GameInstance game) {
+    public Player.PlayerIdentity player1Identity = Player.PlayerIdentity.NONE;
+    public Player.PlayerIdentity player2Identity = Player.PlayerIdentity.NONE;
+
+    public override void Initialize(GameInstance game)
+    {
         if (initialized)
             return;
 
 
         gameInstanceRef = game;
         initialized = true;
+        
+        SetupMenuStartingState();
+        foreach (var level in levelsBundle.Entries)
+        {
+            //Instantiate the level prefab
+            //Set the data e.g.: level name, level description, level image
+        }
     }
 
-    public override void Tick() {
-        if (!initialized) {
+    public override void Tick()
+    {
+        if (!initialized)
+        {
             Error("");
             return;
         }
-
-
-
-
-
-
-
-    }
-    public void SetupMenuStartingState() {
-
-
-
+        
     }
 
+    private bool IsInteractingWithUI()
+    {
+        return EventSystem.current.IsPointerOverGameObject() && Pointer.current.press.isPressed;
+    }
+
+    public void SetupMenuStartingState()
+    {
+        player1Ready = false;
+        player2Ready = false;
+        player1Identity = Player.PlayerIdentity.NONE;
+        player2Identity = Player.PlayerIdentity.NONE;
+    }
 
 
-
-
-
-
-
-
-
-
-    public void DebugLevelButton() {
+    public void DebugLevelButton()
+    {
         gameInstanceRef.StartGame("DebugLevel");
     }
 }
