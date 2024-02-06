d63654248bae43c391a54d60e61e4adfcea172d1
P3Ti009 <gulyaspeter20@gmail.com>
2024-02-06 11:34:33 +0100

    Level Select Menu

--- Assets/Scripts/GUI/LevelSelectMenu.cs
+++ Assets/Scripts/GUI/LevelSelectMenu.cs
@@ -11,20 +11,15 @@ public struct LevelOption {
     public Image progressBar;
     public Image previewImage;
     public Button button;
+    public string name;
+    public string levelKey;
 }
 
 
 public class LevelSelectMenu : Entity {
-
-
+    
     [SerializeField] private GameObject buttonPrefab;
-
-
-    [SerializeField] private ScrollRect scrollRect;
-    [SerializeField] private RectTransform contentRectTransform;
     [SerializeField] private TextMeshProUGUI levelNameText;
-    [SerializeField] private float xCenterOffset = 0.0f;
-    [SerializeField] private Vector2 defaultGuiElementSize = new Vector2(200, 400);
 
     //Ref
     public LevelOption[] levelOptions;
@@ -43,25 +38,12 @@ public class LevelSelectMenu : Entity {
     public string player1LevelVote = "";
     public string player2LevelVote = "";
 
-    //Scrolling
-    private RectTransform canvasRectTransform;
-    private RectTransform[] guiElements;
-    private float canvasCenterX = 0.0f;
-    private float guiElementWidth = 0.0f;
-    private float spacing = 0.0f;
-    private bool isSnapped = false;
-
     //Holding button
     private bool isButtonHeld = false;
     private float timer = 0.0f;
     private float timeToHold = 1.0f;
 
-    private int middleElementIndex = 0;
-
-
-
-
-
+    private int selectedElementIndex = 0;
 
 
     public override void Initialize(GameInstance game) {
@@ -75,8 +57,8 @@ public class LevelSelectMenu : Entity {
 
         initialized = true;
     }
-    public override void Tick()
-    {
+
+    public override void Tick() {
         if (!initialized) {
             Error("");
             return;
@@ -87,14 +69,10 @@ public class LevelSelectMenu : Entity {
             scrollRectComp.content.localPosition = Vector2.zero;
         }
 
-        //ButtonTimer();
-        //UpdateTimerBar();
-        //StopScrollAtEdges();
-        //var currentItem = CalculateCurrentItem();
-        //middleElementIndex = currentItem;
-        //Snap();
-        //UpdateLevelName();
+        ButtonTimer();
+        UpdateTimerBar(selectedElementIndex);
     }
+
     public void SetupMenuStartingState() {
         player1Ready = false;
         player2Ready = false;
@@ -104,26 +82,9 @@ public class LevelSelectMenu : Entity {
     }
 
     private void SetupReferences() {
-        //Time bar
-        //var timerBarTransform = transform.Find("TimerBar");
-        //Validate(timerBarTransform, "Failed to find TimerBar transform", ValidationLevel.ERROR, true);
-        //timeBar = timerBarTransform.GetComponent<Slider>();
-        //Validate(timeBar, "Failed to find TimerBar component", ValidationLevel.ERROR, true);
-        //
-        //timeBar.gameObject.SetActive(false);
-
-
-
         Transform scrollViewTransform = transform.Find("ScrollView");
         scrollRectComp = scrollViewTransform.GetComponent<ScrollRect>();
         layoutGroupComp = scrollRectComp.content.gameObject.GetComponent<HorizontalLayoutGroup>();
-
-
-
-        //Canvas
-        canvasRectTransform = GetComponent<RectTransform>();
-        Validate(canvasRectTransform, "Failed to find Canvas RectTransform", ValidationLevel.ERROR, true);
-        canvasCenterX = canvasRectTransform.rect.width / 2f;
     }
 
     private void SetupGUIElements() {
@@ -134,7 +95,6 @@ public class LevelSelectMenu : Entity {
 
         levelOptions = new LevelOption[levelsBundle.Entries.Length];
         for (int i = 0; i < levelsBundle.Entries.Length + 1; i++) {
-
             GameObject newOption = Instantiate(buttonPrefab, scrollRectComp.content);
             Transform levelButton = newOption.transform.Find("LevelButton");
             if (i == levelsBundle.Entries.Length) {
@@ -145,11 +105,12 @@ public class LevelSelectMenu : Entity {
             }
 
 
-
             LevelOption levelOption = new LevelOption();
             levelOption.progressBar = newOption.GetComponent<Image>();
             levelOption.previewImage = levelButton.GetComponent<Image>();
             levelOption.button = levelButton.GetComponent<Button>();
+            levelOption.name = levelsBundle.Entries[i].name;
+            levelOption.levelKey = levelsBundle.Entries[i].key;
 
             levelOption.progressBar.fillAmount = 0.0f;
             levelOption.previewImage.sprite = levelsBundle.Entries[i].preview;
@@ -157,39 +118,20 @@ public class LevelSelectMenu : Entity {
 
             EventTrigger eventTrigger = levelButton.GetComponent<EventTrigger>();
 
-            EventTrigger.Entry newEvent = new EventTrigger.Entry();
-            newEvent.eventID = EventTriggerType.PointerExit;
-            newEvent.callback.AddListener((eventData) => { OnStartClicking(levelOption.index); });
+            EventTrigger.Entry OnPointerEnter = new EventTrigger.Entry();
+            OnPointerEnter.eventID = EventTriggerType.PointerDown;
+            OnPointerEnter.callback.AddListener((eventData) => { OnStartClicking(levelOption.index); });
             
-            eventTrigger.triggers.Add(newEvent); 
-            
+            EventTrigger.Entry OnPointerExit = new EventTrigger.Entry();
+            OnPointerExit.eventID = EventTriggerType.PointerUp;
+            OnPointerExit.callback.AddListener((eventData) => { OnEndClicking(levelOption.index); });
+
+            eventTrigger.triggers.Add(OnPointerEnter);
+            eventTrigger.triggers.Add(OnPointerExit);
+
             levelOptions[i] = levelOption;
         }
     }
-
-    //private void SetupGUIElements() {
-    //    guiElements = new RectTransform[levelsBundle.Entries.Length];
-    //    var index = 0;
-    //    foreach (var level in levelsBundle.Entries) {
-    //        var levelUIObject = new GameObject();
-    //        
-    //        var levelUIRectTransform = levelUIObject.AddComponent<RectTransform>();
-    //        levelUIRectTransform.SetParent(contentRectTransform);
-    //        levelUIRectTransform.gameObject.name =
-    //            "Button" + index; // Just for testing since all have the same name //level.name;
-    //        levelUIRectTransform.sizeDelta = defaultGuiElementSize;
-    //        guiElements[index] = levelUIRectTransform;
-    //        index++;
-    //
-    //        var levelUIButton = levelUIObject.AddComponent<Button>();
-    //        var levelUIImage = levelUIObject.AddComponent<Image>();
-    //        levelUIImage.sprite = level.preview;
-    //    }
-    //
-    //    guiElementWidth = guiElements[0].sizeDelta.x;
-    //    spacing = contentRectTransform.GetComponent<HorizontalLayoutGroup>().spacing;
-    //    contentRectTransform.GetComponent<ContentSizeFitter>().enabled = false;
-    //}
 
     //Button holding
     private void ButtonTimer() {
@@ -198,12 +140,14 @@ public class LevelSelectMenu : Entity {
 
         timer += Time.deltaTime;
 
-        if (!(timer >= timeToHold))
-            return;
+        if (timer >= timeToHold) {
 
-        TimerOver();
-        timer = 0.0f;
-        isButtonHeld = false;
+            TimerOver();
+            levelOptions[selectedElementIndex].progressBar.fillAmount = 1.0f;
+            levelNameText.text = levelOptions[selectedElementIndex].name;
+            timer = 0.0f;
+            isButtonHeld = false;
+        }
     }
 
     private void TimerOver() {
@@ -211,65 +155,15 @@ public class LevelSelectMenu : Entity {
     }
 
     private void FinalizeVote() {
-        var levelData = levelsBundle.Entries[middleElementIndex];
-        var levelKey = levelData.key;
-        Log(levelKey);
-        //If not selectable return
+        LevelOption levelData = levelOptions[selectedElementIndex];
+        string levelKey = levelData.levelKey;
+        //Log(levelKey);
     }
 
-    public void OnPointerExit() {
-        isButtonHeld = false;
-        timer = 0.0f;
-        timeBar.gameObject.SetActive(false);
-    }
-
-    public void OnPointerEnter() {
-        isButtonHeld = true;
-        timeBar.gameObject.SetActive(true);
-    }
-
-    //Scrollling
-    private void StopScrollAtEdges() {
-        if (guiElements[0].position.x >= canvasCenterX)
-            scrollRect.velocity = Vector2.zero;
-        if (guiElements[^1].position.x <= canvasCenterX)
-            scrollRect.velocity = Vector2.zero;
-    }
-
-    private int CalculateCurrentItem() {
-        var currentItem = Mathf.RoundToInt(-(contentRectTransform.localPosition.x / (guiElementWidth + spacing)));
-        if (currentItem < 0)
-            currentItem = 0;
-        if (currentItem > guiElements.Length - 1)
-            currentItem = guiElements.Length - 1;
-        return currentItem;
-    }
-
-    private void Snap() {
-        if (!isSnapped && scrollRect.velocity.magnitude < 30f) {
-            var localPosition = contentRectTransform.localPosition;
-            var x = middleElementIndex * (guiElementWidth + spacing) + xCenterOffset;
-            var updatedPosition = new Vector3(-x, localPosition.y, localPosition.z);
-
-            localPosition = Vector3.Lerp(localPosition, updatedPosition, 10f * Time.deltaTime);
-            contentRectTransform.localPosition = localPosition;
-
-            isSnapped = Math.Abs(localPosition.x - -x) < 0.1f;
-        }
-        else {
-            isSnapped = false;
-        }
-    }
-
-    //
-    private void UpdateTimerBar() {
+    private void UpdateTimerBar(int index) {
         if (!isButtonHeld)
             return;
-        timeBar.value = timer / timeToHold;
-    }
-
-    private void UpdateLevelName() {
-        levelNameText.text = guiElements[middleElementIndex].name;
+        levelOptions[index].progressBar.fillAmount = timer / timeToHold;
     }
 
     public void DebugLevelButton() {
@@ -277,35 +171,26 @@ public class LevelSelectMenu : Entity {
     }
 
 
-
     public void OnStartClicking(int index) {
-
         LevelOption option = levelOptions[index];
 
+        float middleIndex = (float)levelOptions.Length / 2;
+        float indexDiff = middleIndex - index - .5f;
 
-        float spacingTotal = layoutGroupComp.spacing * index + 1;
-        float totalWidth = option.progressBar.rectTransform.rect.width * index + 1;
-        float anchorPoint = -scrollRectComp.content.rect.width;
+        float spacingTotal = layoutGroupComp.spacing * indexDiff + 1;
+        float totalWidth = option.progressBar.rectTransform.rect.width * indexDiff + 1;
 
-        Vector3 scrollRect = scrollRectComp.content.localPosition;
-        Vector3 resultRect = new Vector3(anchorPoint + (spacingTotal + totalWidth), scrollRect.y, scrollRect.z);
-        scrollRectComp.content.transform.localPosition = resultRect;
-        //Log();
-
-
-        levelNameText.text = levelsBundle.Entries[index].name;
-
-        // bool positive = false;
-        // Vector3 scrollRect = scrollRectComp.content.localPosition;
-        // Vector3 optionRect = option.progressBar.rectTransform.localPosition;
-        // float distanceToOrigin = optionRect.x;
-        //
-        //
-        // Vector3 resultRect = new Vector3(scrollRect.x - distanceToOrigin, scrollRect.y, scrollRect.z);
-        // scrollRectComp.content.transform.localPosition = resultRect;
-        // scrollRectComp.velocity = Vector2.zero;
-
-
-        Log("I received event with index " + index);
+        Vector3 scrollRectPosition = scrollRectComp.content.localPosition;
+        Vector3 resultRect = new Vector3(spacingTotal + totalWidth, scrollRectPosition.y, scrollRectPosition.z);
+        scrollRectComp.content.localPosition = resultRect;
+        scrollRectComp.velocity = Vector2.zero;
+        
+        selectedElementIndex = index;
+        isButtonHeld = true;
+    }
+    
+    public void OnEndClicking(int index) {
+        levelOptions[index].progressBar.fillAmount = 0.0f;
+        isButtonHeld = false;
     }
 }
