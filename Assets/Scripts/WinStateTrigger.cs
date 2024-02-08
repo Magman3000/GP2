313806df036fc64c4a16bc4bf3cef1d05871b7f3
Gammastrale <90925619+Gammastrale@users.noreply.github.com>
2024-02-08 09:50:30 +0100

    Update WinStateTrigger.cs

--- Assets/Scripts/WinStateTrigger.cs
+++ Assets/Scripts/WinStateTrigger.cs
@@ -2,26 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WinStateTrigger : MonoBehaviour
+public class WinStateTrigger : Entity
 {
     //[SerializeField] GameInstance.GameState gameState = GameInstance.GameState.WIN_MENU;
     public GameObject winMenu;
-    /*
+    
     public override void Initialize(GameInstance game)
     {
-        if(initialized)
+        if (initialized)
         {
             return;
         }
+        winMenu = GameObject.Find("WinMenu(Clone)");
+        winMenu.SetActive(false);
         gameInstanceRef = game;
         initialized = true;
     }
-    */
-    public void Start()
-    {
-        winMenu = GameObject.Find("WinMenu(Clone)");
-        winMenu.SetActive(false);
-    }
+    
 
     public void OnTriggerEnter(Collider other)
     {
